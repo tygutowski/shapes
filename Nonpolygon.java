39b94d38d75f8acc42f2d7427cbaea08a8da5d28
Ty Gutowski <tygutowski@gmail.com>
2019-03-12 13:41:13 -0400

    Delete Nonpolygon.java

--- Nonpolygon.java
+++
@@ -1,9 +0,0 @@
-
-public interface Nonpolygon {
-	public final static double PI = 3.14159;
-	public void setName(String name);
-	public String getName();
-	public double getLengMajAxis();
-	public double getLengMinAxis();
-	public double getCircumference();
-}
